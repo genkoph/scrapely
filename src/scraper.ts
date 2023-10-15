9e82d32957b62294804cc43d4e75fd6e387e149b
Kristiyan Borisov <genkoph@protonmail.com>
2023-10-16 01:14:57 +0300

    refactor: reorder functions in scraper.ts

--- src/scraper.ts
+++ src/scraper.ts
@@ -3,23 +3,6 @@ import init from "@/commands/init.command";
 import scope from "@/commands/scope.command";
 import type { Context, Selector } from "@/types";
 
-async function executeCommands(
-  // eslint-disable-next-line
-  commands: [(...args: any[]) => Promise<Context>, Selector][],
-  context: unknown
-): Promise<Selector | null> {
-  const _commands = [...commands];
-  const [current, args] = _commands.shift()!;
-
-  const result = await current(context, args);
-
-  if (_commands.length === 0) {
-    return result.data;
-  }
-
-  return executeCommands(_commands, result);
-}
-
 function scraper(source: string) {
   // eslint-disable-next-line
   const commandsQueue: [(...args: any[]) => Promise<Context>, Selector][] = [
@@ -46,6 +29,23 @@ function scraper(source: string) {
 
 function Scraper() {
   return scraper;
+}
+
+async function executeCommands(
+  // eslint-disable-next-line
+  commands: [(...args: any[]) => Promise<Context>, Selector][],
+  context: unknown
+): Promise<Selector | null> {
+  const _commands = [...commands];
+  const [current, args] = _commands.shift()!;
+
+  const result = await current(context, args);
+
+  if (_commands.length === 0) {
+    return result.data;
+  }
+
+  return executeCommands(_commands, result);
 }
 
 export default Scraper;
