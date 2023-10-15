1f34f0a0b0d6bf35586dffe39f1a9a8737ee98cc
Kristiyan Borisov <genkoph@protonmail.com>
2023-10-16 01:13:43 +0300

    refactor: init command

--- src/commands/init.command.ts
+++ src/commands/init.command.ts
@@ -3,14 +3,10 @@ import superagent from "superagent";
 
 import type { Context } from "@/types";
 
-
-function isUrl(str: string) {
-  const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
-  return urlRegex.test(str);
-}
-
 async function init(source: string): Promise<Context> {
-  if (isUrl(source)) {
+  const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+
+  if (urlRegex.test(source)) {
     const { text } = await superagent.get(source);
     return { $: load(text), data: null, scope: null };
   }
