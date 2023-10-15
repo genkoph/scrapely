0f01966b1b03f51a4bfd195a9195f5b2797994d2
Kristiyan Borisov <genkoph@protonmail.com>
2023-10-15 14:39:35 +0300

    chore: add comment in index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import scraper from "./scraper";
 async function main() {
   const url = "https://www.scrapethissite.com/pages";
 
+  // const scraper = Scraper({
+  //   delay: 20,
+  //   throttle: 80,
+  //   concurrency: 4
+  // });
+
   const data = await scraper(url).data();
 
   console.log(data);
