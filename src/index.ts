646096ddc2c14f2e0922a3cdc838fba82b277202
Kristiyan Borisov <genkoph@protonmail.com>
2023-10-18 01:38:39 +0300

    fix: entry point exports

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
-import Scraper from "./scraper";
+import Scraper from "@/scraper";
 
-async function main() {
-  const url = "https://www.scrapethissite.com/pages/simple";
+export { Scraper };
 
-  const scraper = Scraper();
-
-  const data = await scraper(url).scope(".country").set([{
-    name: ".country-name",
-    capital: ".country-capital",
-    population: ".country-population",
-    area: ".country-area",
-  }]).data();
-
-  console.log(data);
-}
-
-main();
+export type { Context, Selector } from "@/types";
