51894736a0d52bb22695d7a784cd0de5ba8f6df0
Kristiyan Borisov <genkoph@protonmail.com>
2023-10-21 18:41:24 +0300

    chore: fix linting

--- src/methods/scope.method.ts
+++ src/methods/scope.method.ts
@@ -2,7 +2,7 @@ import { Context } from "@/types";
 
 function scope(context: Context, selector: string | string[]): Context {
   const scope = Array.isArray(selector)
-    ? context.root.querySelectorAll(selector[0])
+    ? context.root.querySelectorAll(selector[0]!)
     : context.root.querySelector(selector);
 
   return { ...context, scope };
